472bc747e40b575d17cece7b4b74d3b575e95e42
dgorena88-del <d.gorena.88@gmail.com>
2026-08-15 03:56:58 -0300

    consultas_basicas.sql

--- consultas_basicas.sql
+++ consultas_basicas.sql
@@ -14,10 +14,10 @@ SELECT * FROM sales;
 
 --Consulta 2 - Selección específica:El equipo de finanzas solo necesita identificar al cliente, el producto y el monto--
 
-SELECT customer_id, product_name, total_amount FROM sales;
+SELECT customer_id, product_id, total_amount FROM sales;
 
 ---Consulta 3 — Nombres amigables con alias: El equipo de finanzas no entiende los nombres técnicos en inglés--
 --CAMBIAR: order_date, product_name y quantity renombrándolas con AS como fecha_pedido, nombre_producto y cantidad_unidades respectivamente.--
 
-SELECT order_date AS fecha_pedido, product_name AS nombre_prodcuto, quantity AS cantidad_unidades
+SELECT order_date AS fecha_pedido, product_name AS nombre_producto, quantity AS cantidad_unidades
 FROM sales;
